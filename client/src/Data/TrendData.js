c36c14314241af5da458159e33b6399387d09316
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-09-01 23:09:33 +0530

    softbook app

---
+++ client/src/Data/TrendData.js
@@ -0,0 +1,26 @@
+export const TrendData= [
+  {
+    name: "IPL",
+    shares: 98,
+  },
+  {
+    name: "World Cup",
+    shares: 80.5,
+  },
+  {
+    name: "Covid19",
+    shares: 76.5,
+  },
+  {
+    name: "Reactjs",
+    shares: 74,
+  },
+  {
+    name: "Elon Musk",
+    shares: 71.9,
+  },
+  {
+    name: "NBA",
+    shares: 30,
+  },
+];
